516c3897bb38e22ba19245c1653642ce8aa80e24
Samantha Black <sam@planetary.io>
2017-03-30 15:50:00 -0400

    Remove unnecessary header

--- client/src/components/Paragraph/index.js
+++ client/src/components/Paragraph/index.js
@@ -3,29 +3,23 @@ import styles from './styles.module.css';
 
 export default class Paragraph extends Component {
     static propTypes: {
-        header: PropTypes.string,
         content: PropTypes.string.isRequired
     }
 
+    renderMarkup(data) {
+        return {__html: data};
+    }
+
     render() {
-        const {header, content} = this.props;
+        const {content} = this.props;
 
         return (
             <div className={styles.container}>
-                {header &&
-                    <div className={styles.heading}>
-                        <h2>{header}</h2>
-                    </div>
-                }
                 <div
                     className={styles.content}
                     dangerouslySetInnerHTML={this.renderMarkup(content)}
                 />
             </div>
         );
-    }
-
-    renderMarkup(data) {
-        return {__html: data};
     }
 }
