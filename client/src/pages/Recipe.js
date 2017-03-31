f0243b0ce62e3ec3ed6e44180d54efca42c24bb2
Joshua Gross <joshua@gross.is>
2017-03-31 16:43:50 -0400

    Compose Recipe page from RecipeHead and RecipeStep(s)

--- client/src/pages/Recipe.js
+++ client/src/pages/Recipe.js
@@ -4,6 +4,9 @@ import {Title, preload} from 'react-isomorphic-render';
 
 import {connector, getRecipe} from '../redux/recipe';
 
+import RecipeHead from '../components/API/RecipeHead';
+import RecipeStep from '../components/API/RecipeStep';
+
 @preload(({dispatch, parameters}) => dispatch(getRecipe(parameters.slug)))
 @connect(
     (state) => ({...connector(state.recipe)}),
@@ -11,10 +14,22 @@ import {connector, getRecipe} from '../redux/recipe';
 )
 export default class Recipe extends Component {
     render() {
+        const {recipe} = this.props;
+        const item = recipe.items[0];
+
+        const stepEntries = {};
+        recipe.includes.Entry.forEach((entry) => {
+            if(entry.sys.contentType.sys.id === 'recipeStep')
+                stepEntries[entry.sys.id] = entry;
+        });
+
+        const steps = item.fields.steps.map((step) => stepEntries[step.sys.id]);
+
         return (
             <section className="content">
-                <Title>Recipe</Title>
-                {JSON.stringify(this.props.recipe)}
+                <Title>{`Recipe: ${item.fields.name}`}</Title>
+                <RecipeHead data={recipe} />
+                {steps.map((step) => <RecipeStep data={step} />)}
             </section>
         );
     }
