aee8cfb2129f2866a5a6c2d4965d50575e0453cb
Samantha Black <sam@planetary.io>
2017-04-04 14:51:50 -0400

    Remove test from homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,12 +2,10 @@ import React from 'react';
 import {Title} from 'react-isomorphic-render';
 
 import husky from '../../assets/images/husky.jpg';
-import Button from '../components/Button';
 
 export default () => (
     <section className="content">
         <Title>Home</Title>
-        <Button link="/recipe" style="small">Test</Button>
         <img src={husky} />
     </section>
 );
