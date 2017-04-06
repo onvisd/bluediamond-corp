9ccf8c5864c7c71cbcb4b21cafd0305a402c014d
Samantha Black <sam@planetary.io>
2017-04-06 11:10:18 -0400

    Correct image alt var

--- client/src/components/ImageGallery/index.js
+++ client/src/components/ImageGallery/index.js
@@ -9,7 +9,7 @@ export default class ImageGallery extends Component {
 
     renderGalleryItem(image, id) {
         const url = image.file.url;
-        const alt = image.file.title;
+        const alt = image.title;
 
         return (
             <div key={id} className="l--col-12-at-s l--col-6-at-m l--col-4-at-l">
