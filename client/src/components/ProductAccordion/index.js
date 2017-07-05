480e5e45de92f9bff56a6bea5b9529abaa1f24a5
Joshua Gross <josh@planetary.io>
2017-07-05 12:57:21 -0400

    Render SmartLabel contents like a real nutrition label (#282)
    * Render SmartLabel contents like a real nutrition label
    
    Closes BLUED-123
    
    * Update table to full-width
    
    * Add tbody around nutrition table

--- client/src/components/ProductAccordion/index.js
+++ client/src/components/ProductAccordion/index.js
@@ -31,8 +31,8 @@ export default class ProductAccordion extends Component {
         return (
             <div className={styles.nutrients}>
                 {panels.map((panel, i) =>
-                    <div key={`nutritionPanel${i}`} className={styles.nutrientDetails}>
-                        <p>{panel.name}</p>
+                    <div key={`nutritionPanel${i}`} className={styles.nutritionPanel}>
+                        <h5>{panel.name}</h5>
                         {this.renderNutrientList(panel.nutrients)}
                     </div>
                 )}
@@ -41,14 +41,73 @@ export default class ProductAccordion extends Component {
     }
 
     renderNutrientList(nutrients) {
+        const calories = nutrients.filter(
+            (nutrient) => nutrient.name.match(/Calories( from Fat)?/i)
+        );
+        const totalFat = nutrients.filter(
+            (nutrient) => nutrient.name.match(/Total Fat/i)
+        )[0];
+        const otherFats = nutrients
+            .filter((nutrient) => nutrient.name.match(/(.+?) Fat/))
+            .filter((nutrient) => !nutrient.name.match(/(Total|Calories from) Fat/i));
+        const totalCarbs = nutrients.filter(
+            (nutrient) => nutrient.name.match(/Total Carbohydrate/)
+        )[0];
+        const otherCarbs = nutrients.filter(
+            (nutrient) => nutrient.name.match(/Dietary Fiber|Sugars/)
+        );
+        const otherFacts = nutrients.filter(
+            (nutrient) => !nutrient.name.match(/Calories|Fat|Carbohydrate|Dietary Fiber|Sugars/i)
+        );
+
         return (
-            <div className={styles.nutrient}>
-                {nutrients.map((nutrient, i) =>
-                    <span key={`nurtient${i}`}>
-                        <p>{nutrient.name}, {nutrient.value}, {nutrient.uom}</p>
-                    </span>
-                )}
-            </div>
+            <table className={styles.nutrition}>
+                <tbody>
+                    <tr>
+                        <td>
+                            <strong>{calories[0].name} </strong>
+                            {calories[0].value}
+                        </td>
+                        <td>
+                            {calories[1].name} {calories[1].value}
+                        </td>
+                    </tr>
+                    <tr key="divider">
+                        <td colSpan={2} className={styles.divider}>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td><strong>{totalFat.name}</strong></td>
+                        <td>{totalFat.value}{totalFat.uom}</td>
+                    </tr>
+                    {otherFats.map((fat, i) => (
+                        <tr className={styles.sublist} key={`fats${i}`}>
+                            <td>{fat.name}</td>
+                            <td>{fat.value}{fat.uom}</td>
+                        </tr>
+                    ))}
+                    <tr>
+                        <td><strong>{totalCarbs.name}</strong></td>
+                        <td>{totalCarbs.value}{totalCarbs.uom}</td>
+                    </tr>
+                    {otherCarbs.map((carb, i) => (
+                        <tr className={styles.sublist} key={`fats${i}`}>
+                            <td>{carb.name}</td>
+                            <td>{carb.value}{carb.uom}</td>
+                        </tr>
+                    ))}
+                    {otherFacts.map((nutrient, i) => [
+                        <tr key={`nutrient${i}`}>
+                            <td><strong>{nutrient.name}</strong></td>
+                            <td>{nutrient.value}{nutrient.uom}</td>
+                        </tr>,
+                        nutrient.name === 'Protein' ? (
+                            <tr key="divider">
+                                <td colSpan={2} className={styles.divider}>&nbsp;</td>
+                            </tr>
+                        ) : null
+                    ])}
+                </tbody>
+            </table>
         );
     }
 
