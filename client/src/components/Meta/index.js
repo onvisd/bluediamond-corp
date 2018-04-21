76b117daa7069849ff8c8b4b4cf15586308bd340
Joshua Gross <joshua@gross.is>
2018-04-21 22:06:15 +0100

    Fix property -> name for description

--- client/src/components/Meta/index.js
+++ client/src/components/Meta/index.js
@@ -29,20 +29,23 @@ export default (props) => {
                 'Almond Ratings'
         },
 
+        {
+            name: 'description',
+            content: 'From our hearts to your hands'
+        },
+
         // <meta property="..." content="..."/>
         // OpenGraph Tags
         {property: 'og:title', content: 'Blue Diamond Growers'},
         {property: 'og:description', content: 'From our hearts to your hands'},
-        {property: 'og:locale', content: 'en-US'},
-
-        // Other Meta Property Tags
-        {property: 'description', content: 'From our hearts to your hands'}
+        {property: 'og:locale', content: 'en-US'}
     ];
 
     // Overwrite defaults
     const meta = defaults.map((tag) => {
         const matchingTag = tags.filter(
-            (childTag) => childTag.property === tag.property
+            (childTag) =>
+                (childTag.property === tag.property || childTag.name === tag.name)
         )[0];
 
         if(matchingTag)
@@ -54,12 +57,21 @@ export default (props) => {
     // Add any new tags
     tags.forEach((tag) => {
         const matchingTag = meta.filter(
-            (existingTag) => existingTag.property === tag.property
+            (existingTag) =>
+                (existingTag.property === tag.property || existingTag.name === tag.name)
         )[0];
 
         if(!matchingTag)
             meta.push(tag);
     });
 
-    return (<IsomorphicMeta>{meta}</IsomorphicMeta>);
+    const keys = [];
+    return (<IsomorphicMeta>{meta.filter((tag) => {
+        const key = `${tag.property ? 'p' : ''}${tag.property || tag.name}`;
+        if(keys.indexOf(key) > -1)
+            return false;
+
+        keys.push(key);
+        return true;
+    })}</IsomorphicMeta>);
 };
