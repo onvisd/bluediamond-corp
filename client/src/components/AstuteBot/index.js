93a24750276b057626afeeaf906b5cd369e54f63
Andrew Coelho <acoelho.baa@gmail.com>
2018-07-30 21:53:18 -0700

    Update astute bot styles

--- client/src/components/AstuteBot/index.js
+++ client/src/components/AstuteBot/index.js
@@ -1,6 +1,9 @@
 /* eslint-disable max-len */
 import {Component} from 'react';
 
+import AstuteLauncher from 'images/astute-launcher.png';
+import BDIcon from 'images/icon.png';
+
 export default class AstuteBot extends Component {
     componentDidMount() {
         const script = document.createElement('script');
@@ -16,12 +19,11 @@ export default class AstuteBot extends Component {
                 autoGetStarted: true,
                 persistConversationAcrossSession: false,
                 replyHeightPixels: 50,
-                toggleChatMarkup: 'Need Help?',
-                titleMarkup: 'Blue Diamond Help',
+                toggleChatMarkup: '<img src="${AstuteLauncher}" style="height: 70px;" />',
+                titleMarkup: '<img src="${BDIcon}" style="margin-right: 6px;" /> Chatting with Blue Diamond',
                 initialContext: JSON.stringify([
                     {
                         userInfo: {
-
                             language: 'en'
                         },
                     }
@@ -29,10 +31,10 @@ export default class AstuteBot extends Component {
                 customCss:
                     '@import url("https://use.typekit.net/pru3xpz.css");' +
                     '.AstuteBotLauncher { position: fixed; bottom: 0px; right: 20px;} ' +
-                    '.AstuteBotLauncher .ToggleChat { float: right; margin-right: 100px; background-color: #fdbb30; color: #2b3b48; padding: 0 20px; font-family: "brandon-grotesque", sans-serif; font-weight: 400; font-size: 16px; height: 40px; line-height: 40px; } '  +
+                    '.AstuteBotLauncher .ToggleChat { float: right; margin-bottom: 30px; color: #2b3b48; padding: 0 20px; font-family: "brandon-grotesque", sans-serif; font-weight: 400; font-size: 16px; height: 40px; line-height: 40px; } '  +
                     'div.AstuteBotLauncher .ChatTitleBar { background-color: #fdbb30; color: #2b3b48; height: 40px; line-height: 40px;} ' +
-                    'div.AstuteBotLauncher .ChatTitleBar .TitleText { font-family: "brandon-grotesque", sans-serif; font-weight: 400; font-size: 16px; margin-left: 10px} ' +
-                    'div.AstuteBotLauncher .ChatTitleBar .MinMaxChat { margin-right: 20px} ',
+                    'div.AstuteBotLauncher .ChatTitleBar .TitleText { font-family: "brandon-grotesque", sans-serif; font-weight: 400; font-size: 16px; margin-left: 10px; display: inline-flex; align-items: center; } ' +
+                    'div.AstuteBotLauncher .ChatTitleBar .MinMaxChat { margin-right: 20px; }',
 
                 botUrl: 'https://www.astutebot.com/chat/index.aspx?aid=QiGxMOTLTz2a4sfhRdS%2BDA%3D%3D'
             });
