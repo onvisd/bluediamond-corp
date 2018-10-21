77ee689a401ee5ffc8ba8518d1965b01780ac91c
Sarabjeet Dhillon <sarabjeet.impinge@gmail.com>
2018-10-21 22:27:31 +0530

    feat: API route to get holiday pages assets
    Code don't have any catches for errors yet; this is just a POC because the
    contentful account provided by client don't have any permissions to create
    anything, so we are trying to just get contentful integrated using whatever
    stuff is already present in the dev account on contentful.

---
+++ api/api/Holiday.js
@@ -0,0 +1,36 @@
+const getHolidayPage = async (client) => {
+    const entries = await client.getEntries({
+        content_type: 'page',
+        limit: 1,
+        query: 'Whole+Natural+Almonds'
+    });
+
+    if (!entries.items || entries.items.length !== 1) {
+        throw new Error('ContentfulPageNotFound: Whole Natural Almonds');
+    }
+
+    const page = entries.items[0];
+
+    return page;
+};
+
+const getHeroImage = async (holidayPage) => {
+    const heroModule = holidayPage.fields.modules.find(
+        (m) => m.sys.contentType.sys.id === 'pageModuleGenericHero'
+    );
+    const heroImage = heroModule.fields.backgroundImage.fields.file.url;
+
+    return heroImage;
+};
+
+export default (api) => {
+    api.get('/holiday/banners', async (req, res) => {
+        const client = req.client;
+        const holidayPage = await getHolidayPage(client);
+        const heroImage = await getHeroImage(holidayPage);
+
+        res.send({
+            heroImage
+        });
+    });
+};
