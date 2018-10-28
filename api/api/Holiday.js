19fbacfcb1a6e59b3f77dd8996f11bf0d27eea6b
Sarabjeet Dhillon <sarabjeet.impinge@gmail.com>
2018-10-28 19:12:43 +0530

    feat: update holidays content payload as per new changes in contentful

--- api/api/Holiday.js
+++ api/api/Holiday.js
@@ -2,8 +2,8 @@ const getHolidayPage = async (client) => {
     const entries = await client.getEntries({
         content_type: 'page',
         limit: 1,
-        query: 'Whole+Natural+Almonds'
-    })
+        query: 'Holidays'
+    });
 
     if (!entries.items || entries.items.length !== 1) {
         throw new Error('ContentfulPageNotFound: Whole Natural Almonds');
@@ -14,26 +14,46 @@ const getHolidayPage = async (client) => {
     return page;
 };
 
-const getHeroImage = async (holidayPage) => {
+const getHeroModuleContent = async (holidayPage) => {
     const heroModule = holidayPage.fields.modules.find(
-        (m) => m.sys.contentType.sys.id === 'pageModuleGenericHero'
+        (m) => m.sys.contentType.sys.id === 'pageModuleHero'
     );
-    const heroImage = heroModule.fields.backgroundImage.fields.file.url;
+    const backgroundImageUrl = heroModule.fields.backgroundImage.fields.file.url;
+    const content = heroModule.fields.content;
 
-    return heroImage;
+    return {
+        backgroundImageUrl,
+        content
+    };
+};
+
+const getGalleryContent = async (holidayPage) => {
+    const galleryModule = holidayPage.fields.modules.find(
+        (m) => m.sys.contentType.sys.id === 'pageModuleGallery'
+    );
+    const images = galleryModule.fields.images.map((image) => {
+        return {
+            url: image.fields.file.url,
+            title: image.fields.title
+        };
+    });
+
+    return {
+        images
+    };
 };
 
 export default (api) => {
-    api.get('/holidays/assets', async (req, res) => {
-        debugger;
+    api.get('/holidays/content', async (req, res) => {
         const client = req.client;
         const holidayPage = await getHolidayPage(client);
-        const heroImage = await getHeroImage(holidayPage);
+
+        const hero = await getHeroModuleContent(holidayPage);
+        const gallery = await getGalleryContent(holidayPage);
 
         res.send({
-            heroImage
+            hero,
+            gallery
         });
     });
 };
-
-
